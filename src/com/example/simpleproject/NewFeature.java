03291cc8dcee525d82aaee787245f328f26b5316
mkrishnan <vmkrishnan91@gmail.com>
2023-03-18 22:47:17 +0530

    Changed NewFeature.java

--- src/com/example/simpleproject/NewFeature.java
+++ src/com/example/simpleproject/NewFeature.java
@@ -3,5 +3,6 @@ package com.example.simpleproject;
 public class NewFeature {
     public static void main(String[] args) {
         System.out.println("New Feature");
+        System.out.println("Another commit to feature branch");
     }
 }
